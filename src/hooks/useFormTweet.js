c566fdf6aed861a9ee692c2fa1374b6625bf6a28
Daniel Montoya <dmontoya.lab@gmail.com>
2021-02-16 08:50:13 -0500

    Hook para controlar formulario

---
+++ src/hooks/useFormTweet.js
@@ -0,0 +1,14 @@
+import { ref } from 'vue';
+
+export default function useFormTweet(){
+    const showForm = ref(false);
+
+    const openClosedForm = () => {
+        showForm.value = !showForm.value;
+    };
+
+    return {
+        showForm,
+        openClosedForm
+    };
+};
